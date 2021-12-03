e4d979c4d967b86e6f3f344f1f317b4fdb1e7799
RojhatSefdin <Rojhat10@hotmail.com>
2021-12-03 06:19:15 +0100

    server crash   return typeof res.headersSent !== 'boolean' no clue ?

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require('express')
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
 const app = express();
+// setting upp our server to connect to socket.io directly
+const server = http.createServer(app); 
+// inizilizing io 
+const io = socketio(server); 
+
+// set our static folder to public 
+app.use(express.static(path.join(__dirname, "public"))); 
+// run when a client connect to the server 
+io.on('connection', socket =>{
+  console.log('New WS Connection...'); 
+}); 
 
 // we set port to 3000 or it checkes for a port 
 const PORT = 3000 || process.env.port; 
-// app listens to port, and gives us a messege log in console when it runs 
-app.listen(PORT, ()=> console.log(`server running on ${PORT}`)); 
+// we use server instead of app for listens to port, and gives us a messege log in console when it runs 
+server.listen(PORT, ()=> console.log(`server running on ${PORT}`)); 
