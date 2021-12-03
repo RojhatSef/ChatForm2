0bedd969c4f76780c4729cb9390d2df1f6e05fea
RojhatSefdin <Rojhat10@hotmail.com>
2021-12-03 05:56:58 +0100

    setting up port and express again

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-const app = require('express')();
-const http = require('http').Server(app);
+const express = require('express')
+const app = express();
+
+// we set port to 3000 or it checkes for a port 
+const PORT = 3000 || process.env.port; 
+// app listens to port, and gives us a messege log in console when it runs 
+app.listen(PORT, ()=> console.log(`server running on ${PORT}`)); 
