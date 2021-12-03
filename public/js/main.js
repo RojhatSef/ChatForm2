a6d04324f24f859cada5ae0df7c37567dfeab6b6
RojhatSefdin <Rojhat10@hotmail.com>
2021-12-03 07:14:44 +0100

    fixed where the messege wasn't sent to console again, no idea what made it happen

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,18 @@
+// to post code to our front-end we grab the id and change the html 
+const chatForm = document.getElementById('chat-form'); 
 const socket = io(); 
 // our emit from server we handle it here 
 // whenever we get the messege event we send this paramter 
-
+// (3) emit to the website our message
 socket.on('message', message =>{
     console.log(message); 
 }); 
+// we wait for the submit button to happen, when it happens we send our changes back to the server
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+  
+    // Get message text
+    const msg = e.target.elements.msg.value;
+    socket.emit('chatMessage', msg);
+    console.log(msg);
+});
